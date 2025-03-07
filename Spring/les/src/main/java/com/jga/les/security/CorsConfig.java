39489792b61389943ca5fb9dd3496a9d8a87bfa7
Gabriel Garcia Tissianel <gabriel.gtissianel@gmail.com>
2025-03-07 16:55:35 -0300

    spring: gitignore e update no properties

--- Spring/les/src/main/java/com/jga/les/security/CorsConfig.java
+++ Spring/les/src/main/java/com/jga/les/security/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:44129")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
