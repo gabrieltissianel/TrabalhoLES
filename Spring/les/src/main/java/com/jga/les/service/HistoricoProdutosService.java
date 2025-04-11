e4c4a8982f7e996275163b255ce2d8d6d5012fd2
Joao <joaopcorreia014@gmail.com>
2025-04-11 18:28:56 -0300

    update:  list por produto, historico de produtos

--- Spring/les/src/main/java/com/jga/les/service/HistoricoProdutosService.java
+++ Spring/les/src/main/java/com/jga/les/service/HistoricoProdutosService.java
@@ -1,8 +1,13 @@
 package com.jga.les.service;
 
 import com.jga.les.model.HistoricoProdutos;
+import com.jga.les.model.Produto;
+import com.jga.les.repository.HistoricoProdutosRepository;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +15,9 @@ public class HistoricoProdutosService extends GenericService<HistoricoProdutos> 
 
     public HistoricoProdutosService(JpaRepository<HistoricoProdutos, Long> objRepository) {
         super(objRepository, HistoricoProdutos.class);
+    }
+
+    public ResponseEntity<List<Produto>> findByProduto(Long produto_id) {
+        return ResponseEntity.ok(((HistoricoProdutosRepository) objRepository).findByProdutoId(produto_id));
     }
 }
